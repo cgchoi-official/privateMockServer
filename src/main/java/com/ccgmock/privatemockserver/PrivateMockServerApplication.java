7dd4fa828d9162e0d1a57c71ad947575ae947557
cgchoi <cgchoi@fssuniverse.com>
2022-05-15 15:09:47 +0900

    Initial commit

---
+++ src/main/java/com/ccgmock/privatemockserver/PrivateMockServerApplication.java
@@ -0,0 +1,13 @@
+package com.ccgmock.privatemockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateMockServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateMockServerApplication.class, args);
+    }
+
+}
